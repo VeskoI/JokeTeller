610e98e183633a8a1e5869058bfb58ea8b7ed7a9
Vesselin Iliev <iliev.vesselin@gmail.com>
2015-06-29 01:33:47 +0300

    Add GCE module. It fetches jokes from the java library. The app displays the joke via the Activity in the Jokes Android Library. NOTE - changed the GCE port to 8081 (the detault was used by other programs on my pc)

---
+++ backend/src/main/java/com/example/vesko/myapplication/backend/MyEndpoint.java
@@ -0,0 +1,33 @@
+/*
+   For step-by-step instructions on connecting your Android application to this backend module,
+   see "App Engine Java Endpoints Module" template documentation at
+   https://github.com/GoogleCloudPlatform/gradle-appengine-templates/tree/master/HelloEndpoints
+*/
+
+package com.example.vesko.myapplication.backend;
+
+import com.google.api.server.spi.config.Api;
+import com.google.api.server.spi.config.ApiMethod;
+import com.google.api.server.spi.config.ApiNamespace;
+import com.jokes.example.JokeTeller;
+
+import javax.inject.Named;
+
+/**
+ * An endpoint class we are exposing
+ */
+@Api(name = "myApi", version = "v1", namespace = @ApiNamespace(ownerDomain = "backend.myapplication.vesko.example.com", ownerName = "backend.myapplication.vesko.example.com", packagePath = ""))
+public class MyEndpoint {
+
+    /**
+     * A simple endpoint method that takes a name and says Hi back
+     */
+    @ApiMethod(name = "getJoke")
+    public MyBean getJoke() {
+        MyBean response = new MyBean();
+        response.setData("Your joke for today is:\n" + JokeTeller.getJoke());
+
+        return response;
+    }
+
+}
