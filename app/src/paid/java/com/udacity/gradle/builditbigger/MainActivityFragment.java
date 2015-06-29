a0e02965e567e6974573cf3026efe2bd2d53088b
Vesselin Iliev <iliev.vesselin@gmail.com>
2015-06-29 16:03:55 +0300

    Add FREE and PAID build flavors. Ads are only displayed in the FREE flavor

---
+++ app/src/paid/java/com/udacity/gradle/builditbigger/MainActivityFragment.java
@@ -0,0 +1,25 @@
+package com.udacity.gradle.builditbigger;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * Simple placeholder fragment.
+ *
+ * NOTE - this is the PAID version, so we don't initialise any ads here.
+ */
+public class MainActivityFragment extends Fragment {
+
+    public MainActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_main, container, false);
+    }
+}
